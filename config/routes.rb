fd66dcfec8e4e812dcc8621ed359c7ebbf1f0dcc
SmartDevSource <dreamrock@hotmail.fr>
2023-05-16 14:23:16 +0200

    u

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'events/index'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :events
 
   # Defines the root path route ("/")
   root "events#index"
